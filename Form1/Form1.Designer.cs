2da3a56c8c009364e58e1338d39cb8ce1ff457e4
Cristian Ramirez <cristian.ramirez@uhispano.ac.cr>
2016-01-28 13:15:48 -0600

    Lab - clase 3 - 25 Enero
    Laboratorio clase 3 Lunes 25 Enero

---
+++ Form1/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Form1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtInt = new System.Windows.Forms.TextBox();
+            this.txtHello = new System.Windows.Forms.TextBox();
+            this.txtArray = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(35, 45);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(83, 16);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Press Enter";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(38, 79);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(307, 79);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 55);
+            this.btnEnter.TabIndex = 2;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(307, 149);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 50);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(307, 217);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 57);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtInt
+            // 
+            this.txtInt.Location = new System.Drawing.Point(38, 135);
+            this.txtInt.Name = "txtInt";
+            this.txtInt.Size = new System.Drawing.Size(100, 20);
+            this.txtInt.TabIndex = 5;
+            // 
+            // txtHello
+            // 
+            this.txtHello.Location = new System.Drawing.Point(38, 192);
+            this.txtHello.Name = "txtHello";
+            this.txtHello.Size = new System.Drawing.Size(100, 20);
+            this.txtHello.TabIndex = 6;
+            // 
+            // txtArray
+            // 
+            this.txtArray.Location = new System.Drawing.Point(38, 246);
+            this.txtArray.Name = "txtArray";
+            this.txtArray.Size = new System.Drawing.Size(100, 20);
+            this.txtArray.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 320);
+            this.Controls.Add(this.txtArray);
+            this.Controls.Add(this.txtHello);
+            this.Controls.Add(this.txtInt);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtInt;
+        private System.Windows.Forms.TextBox txtHello;
+        private System.Windows.Forms.TextBox txtArray;
+    }
+}
+
---
+++ Form1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string tag = "Hello World";
+        private bool content = true;
+        private Int32 i = 35;
+        private string mystring = "Hello World";
+        private int[] myInts = { 5, 10, 15, 20 };
+       
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            txtNombre.Text = content.ToString();
+            btnClear.Enabled = true;
+            txtInt.Text = i.ToString();
+            txtHello.Text = mystring;
+
+            string nuevo="";
+            //for (int j = 0; j < myInts.Length; j++ )
+            //nuevo +=  myInts[j].ToString() + " - ";
+            //txtArray.Text = nuevo;
+
+            foreach (int array in myInts)
+            {
+                int j = 0;
+                nuevo += array + " - ";
+                txtArray.Text = nuevo.Substring(0, nuevo.Length - 3);
+                j++;
+            }
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtNombre.Text = "";//txtNombre.Text.Substring(0, txtNombre.Text.Length - 1);
+            txtInt.Text = "";
+            txtHello.Text = "";
+            txtArray.Text = "";
+            btnClear.Enabled = false;
+                       
+
+
+        }
+
+
+
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnClear.Enabled = false;
+        }
+    }
+}
